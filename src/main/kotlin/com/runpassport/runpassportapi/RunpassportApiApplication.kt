124188a0d65860397fcffc14027fe1a9c093837c
minjun <hitobi1014@gmail.com>
2026-08-10 11:08:17 +0900

    chore: first commit

---
+++ src/main/kotlin/com/runpassport/runpassportapi/RunpassportApiApplication.kt
@@ -0,0 +1,11 @@
+package com.runpassport.runpassportapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RunpassportApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RunpassportApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/runpassport/runpassportapi/RunpassportApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.runpassport.runpassportapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RunpassportApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
